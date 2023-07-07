88c0e974ee3a25f82223c36489fb15e2ff0c14a7
Carl <meatballsofmeatballs@gmail.com>
2023-07-07 20:58:47 +0200

    Fixes

--- bin/test-exercises.sh
+++ bin/test-exercises.sh
@@ -1,23 +1,22 @@
 dic=$(mktemp -d)
-mkdir -p "${dic}/src"
-mkdir -p "${dic}/spec"
-echo $(crystal -v)
+mkdir -p "${dic}"
 
 test_run() {
     echo "Testing $1"
-    cat "$1/.meta/src/$2.cr" > "${dic}/src/$2.cr"
+    source_file="$1/$(jq -r '.files.solution[0]' $1/.meta/config.json)"
     spec_file="$1/$(jq -r '.files.test[0]' $1/.meta/config.json)"
-    cat "${spec_file}" > "${dic}/spec/spec.cr"
-    sed -i -e 's/xit/it/g' ${dic}/spec/spec.cr
+    cat "$1/.meta/example.coffee" > "${dic}/${source_file}"
+    cat "${spec_file}" > "${dic}/${spec_file}"
+    sed -i -e 's/xit/it/g' ${dic}/${spec_file}
     jasmine-node --coffee *spec* || exit 1
 }
 
 for exercise in ./exercises/practice/*; do
-    test_run ${exercise} "example"
+    test_run ${exercise} 
 done
 
 if [ -d "./exercises/concept" ]; then
     for exercise in ./exercises/concept/*; do
-        test_run ${exercise} "exemplar"
+        test_run ${exercise}
     done
 fi
