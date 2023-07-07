b384a93d9d3ce7a39308cca94884d5e8f9e7b134
Carl <meatballsofmeatballs@gmail.com>
2023-07-07 21:10:46 +0200

    Fixed filepath

--- bin/test-exercises.sh
+++ bin/test-exercises.sh
@@ -1,12 +1,11 @@
-dic=$(mktemp -d)
-mkdir -p "${dic}"
-
 test_run() {
+    dic=$(mktemp -d)
     echo "Testing $1"
-    source_file="$1/$(jq -r '.files.solution[0]' $1/.meta/config.json)"
-    test_file="$1/$(jq -r '.files.test[0]' $1/.meta/config.json)"
+    source_file="$(jq -r '.files.solution[0]' $1/.meta/config.json)"
+    test_file="$(jq -r '.files.test[0]' $1/.meta/config.json)"
+    echo "source_file: ${source_file}"
     cat "$1/.meta/example.coffee" > "${dic}/${source_file}"
-    cat "${test_file}" > "${dic}/${test_file}"
+    cat "$1/${test_file}" > "${dic}/${test_file}"
     sed -i -e 's/xit/it/g' ${dic}/${test_file}
     jasmine-node --coffee "${dic}/${test_file}" || exit 1
 }
