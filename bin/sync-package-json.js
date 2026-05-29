ed21433e16e64930f9668c9664e676b886925d6e
BNAndras <20251272+BNAndras@users.noreply.github.com>
2026-05-28 20:07:46 -0700

    Update CI

--- bin/sync-package-json.js
+++ bin/sync-package-json.js
@@ -3,17 +3,29 @@ const path = require('path');
 
 const root = path.resolve(__dirname, '..');
 const pkg = require('../package.json');
-delete pkg.private;
+
+const exercisePkg = {
+    name: pkg.name,
+    description: pkg.description,
+    author: pkg.author,
+    contributors: pkg.contributors,
+    license: pkg.license,
+    repository: pkg.repository,
+    devDependencies: {
+        'coffeescript': pkg.devDependencies['coffeescript'],
+        'jasmine-node': pkg.devDependencies['jasmine-node']
+    },
+    scripts: {
+        test: pkg.scripts.test,
+        watch: pkg.scripts.watch
+    },
+    packageManager: pkg.packageManager
+};
 
 fs.readdirSync(path.join(root, 'exercises/practice')).forEach(slug => {
     const dir = path.join(root, 'exercises/practice', slug);
     if (!fs.statSync(dir).isDirectory()) return;
 
-    const newPkg = {
-        ...pkg,
-        repository: { ...pkg.repository, directory: `exercises/practice/${slug}` }
-    };
-
-    fs.writeFileSync(path.join(dir, 'package.json'), JSON.stringify(newPkg, null, 2) + '\n');
+    fs.writeFileSync(path.join(dir, 'package.json'), JSON.stringify(exercisePkg, null, 2) + '\n');
     console.log(`Synced ${slug}`);
 });
